dab26912a710e769b799fdff76a41e98dec7fe4f
Diego Paraguai <107950626+dihparaguai@users.noreply.github.com>
2024-02-12 10:36:54 -0300

    Create DrawingBook.cs

---
+++ DrawingBook.cs
@@ -0,0 +1,85 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+/*
+A teacher asks the class to open their books to a page number. A student can either start turning pages from the front of the book or from the back of the book. They always turn pages one at a time. When they open the book, page 1 is always on the right side
+
+When they flip page 1, they see pages 2 and 3. Each page except the last page will always be printed on both sides. The last page may only be printed on the front, given the length of the book. If the book is n pages long, and a student wants to turn to page p, what is the minimum number of pages to turn? They can start at the beginning or the end of the book.
+
+Given n and p, find and print the minimum number of pages that must be turned in order to arrive at page p.
+
+Sample Input 0
+6
+2
+
+Sample Output 0
+1
+
+*/
+
+class Result
+{
+
+    /*
+     * Complete the 'pageCount' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER p
+     */
+
+    public static int pageCount(int maxPages, int page)
+    {
+        int pagesDistanceFromFinalPage = maxPages - page;
+        int turnPage = 0;
+        
+        if (pagesDistanceFromFinalPage >= page)
+        {
+            turnPage = page/2;
+        }
+        else 
+        {
+            if (pagesDistanceFromFinalPage == 1 && maxPages % 2 == 0)
+            {
+                turnPage = 1;
+            }
+            else
+            {
+            turnPage = pagesDistanceFromFinalPage/2;
+            }
+        }
+        return turnPage;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        int p = Convert.ToInt32(Console.ReadLine().Trim());
+
+        int result = Result.pageCount(n, p);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
